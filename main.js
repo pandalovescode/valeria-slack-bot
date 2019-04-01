dc1971dfca38908582c76e7bbf488ac0b5dd1402
Valeria Gazizova <vgazizova@MBP-VGAZIZOVA.local>
2019-04-01 11:10:32 +0300

    reorder

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const request = require('request');
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   const reply = {
@@ -15,9 +18,7 @@ app.post('/action-endpoint', function (req, res) {
   };
   res.json(reply);
 });
-const request = require('request');
 
-app.use(bodyParser.json());
 
 const listener = app.listen(process.env.PORT || '3000', function () {
   console.log('Your app is listening on port ' + listener.address().port);
