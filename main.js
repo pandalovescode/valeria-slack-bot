03de61275bbc8e30c234b8e2a6dfdbcb7db454e6
Valeria Gazizova <vgazizova@MBP-VGAZIZOVA.local>
2019-04-01 11:38:48 +0300

    change order

--- main.js
+++ main.js
@@ -20,10 +20,6 @@ app.post('/action-endpoint', function (req, res) {
 });
 
 
-const listener = app.listen(process.env.PORT || '3000', function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
-
 app.post('/action-endpoint', function (req, res) {
   const challenge = req.body.challenge; // this challenge is needed to ensure slack that our bot works
   
@@ -69,4 +65,8 @@ app.post('/action-endpoint', function (req, res) {
     });
   }
   res.json(reply);
+});
+
+const listener = app.listen(process.env.PORT || '3000', function () {
+  console.log('Your app is listening on port ' + listener.address().port);
 });
