de9b7a837ed449a71f504ad54cda39c392a29ee1
Valeria Gazizova <vgazizova@MBP-VGAZIZOVA.local>
2019-04-01 11:26:06 +0300

    add currency

--- main.js
+++ main.js
@@ -24,4 +24,49 @@ const listener = app.listen(process.env.PORT || '3000', function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
 
+app.post('/action-endpoint', function (req, res) {
+  const challenge = req.body.challenge; // this challenge is needed to ensure slack that our bot works
   
+  const reply = {
+      "challenge": challenge
+  };
+
+  const headers = {
+    'Content-type': 'application/json',
+    'Authorization': `Bearer ${process.env.TOKEN}` // this token you need to set on heroku
+  }
+
+  // console.log(req.body.event);
+
+  if (req.body.event.subtype != 'bot_message') { // se we won't reply to ourselves...
+    request.get('https://api.coindesk.com/v1/bpi/currentprice/EUR.json', function(err, res, body) {
+      if (err) {
+        console.log(err);
+      }
+      else {
+        const coindesk = JSON.parse(body);
+        const rate = coindesk.bpi.EUR.rate;
+        const reply = {
+          'channel': req.body.event.channel,
+          text: `Current BTC rate: ${rate} EUR per 1 BTC`
+        }
+
+        const options = {
+          url:   'https://slack.com/api/chat.postMessage',
+          method: 'POST',
+          headers,
+          body:  JSON.stringify(reply)
+        };
+
+        console.log(body);
+    
+        request.post(options, function(err, res, body) {
+          if (err) {
+            console.log(err);
+          }
+        });
+      }
+    });
+  }
+  res.json(reply);
+});
